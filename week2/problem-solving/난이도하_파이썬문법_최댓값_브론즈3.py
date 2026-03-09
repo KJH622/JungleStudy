87ac0e5851bb1a8f3b9baf130ae4af172923e186
jihyun <russee201916545@gmail.com>
2026-03-09 11:01:49 +0900

    update: 2562-최댓값

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,12 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+lst = []
+
+for _ in range(9):
+    n = int(input())
+    lst.append(n)
+
+print(max(lst))
+
+print(lst.index(max(lst)) + 1)
