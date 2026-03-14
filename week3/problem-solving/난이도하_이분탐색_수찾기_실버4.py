4acbc5359b0c7f5121c51ab8989f8cab3f4bc9ac
jihyun <russee201916545@gmail.com>
2026-03-14 12:52:02 +0900

    feat: problem-solving_난이도하_이분탐색_수찾기_실버4

--- week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,34 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+
+# N개의 정수에 X라는 정수가 존재하는지 알아내는 프로그램
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+m = int(input())
+
+target = list(map(int, input().split()))
+
+for i in target:
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == i:
+            print(1)
+            break
+        elif arr[mid] > i:
+            right = mid - 1
+        else:
+            left = mid + 1
+        
+    else:
+        print(0)
