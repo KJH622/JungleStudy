6cd015fa5eb3cb503b4849ddcc32749341e18f8f
jihyun <russee201916545@gmail.com>
2026-03-21 09:35:30 +0900

    feat: problem-solving_난이도하_트리_상근이의여행_실버4

--- week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,15 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+
+T = int(input()) # 테스트 케이스 수
+
+# n = 국가의 수
+# m = 비행기 종류
+
+for _ in range(T):
+    n, m = map(int, input().split())
+
+    for _ in range(m):
+        input()
+        
+    print(n-1)
