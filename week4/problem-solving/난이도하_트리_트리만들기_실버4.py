9227f7017a10d49165d78a1d8f5891a687259d97
jihyun <russee201916545@gmail.com>
2026-03-21 09:32:56 +0900

    feat: problem-solving_난이도 하_트리_트리만들기_실버4

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,13 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+# n = 노드 개수
+# m = 리프 개수
+
+n, m = map(int, input().split())
+
+for i in range(1, m+1):
+    print(f'0 {i}')
+
+for j in range(m, n-1):
+    print(f'{j} {j+1}')
