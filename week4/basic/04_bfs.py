84dc49cf185bbadcd6d40e6c99a5cb6ad66fb462
jihyun <russee201916545@gmail.com>
2026-03-22 22:00:54 +0900

    feat: 04_bfs_graph로 탐색

--- week4/basic/04_bfs.py
+++ week4/basic/04_bfs.py
@@ -44,18 +44,28 @@ def bfs(graph, start):
         방문 순서 리스트
     """
     visited = []
+    order = []
     
     # TODO: 큐 생성 및 시작 정점 추가
     ## 방문한 정점 집합
-    pass
+    queue = deque()
+    queue.append(start)
+    visited.append(start)
 
     # TODO: 큐가 빌 때까지 반복
     ## 큐에서 정점 꺼내기
     ## 인접한 정점들 확인
     ## 방문하지 않은 정점이면 큐에 추가
-    pass
+    while queue:
+        current = queue.popleft()
+        order.append(current)
+        for neighbor in graph[current]: 
+            if neighbor not in visited:
+                visited.append(neighbor)
+                queue.append(neighbor)
+
     
-    return visited
+    return order
 
 # 테스트 케이스
 if __name__ == "__main__":
@@ -71,4 +81,3 @@ if __name__ == "__main__":
     result = bfs(graph, 0)
     print(f"시작 정점: 0")
     print(f"방문 순서: {result}")
-
