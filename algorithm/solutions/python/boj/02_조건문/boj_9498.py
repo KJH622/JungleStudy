78618c02c9abb9b9e39ae923b96affb875bbc819
jihyun <russee201916545@gmail.com>
2026-03-23 16:22:03 +0900

    docs(boj): add record and solution for 9498

---
+++ algorithm/solutions/python/boj/02_조건문/boj_9498.py
@@ -0,0 +1,15 @@
+# BOJ 9498 - 시험 성적
+# 핵심: 90~100점은 A, 80~89점은 B, 70~79점은 C, 60~69점은 D, 나머지는 F
+
+score = int(input())
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("F")
