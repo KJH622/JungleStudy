a3693b3515a1799b550bb899ae791c59659e95f7
jihyun <russee201916545@gmail.com>
2026-03-27 23:11:03 +0900

    feat: problem-solving_난이도하_DP_피보나치수2_브론즈1

--- week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,23 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+
+def fibo(n, memo=None):
+
+    if memo is None:
+        memo = {}
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibo(n-1, memo) + fibo(n-2, memo)
+
+    return memo[n]
+
+n = int(input())
+
+print(fibo(n))
