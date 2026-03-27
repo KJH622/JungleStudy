8eb544b338410c4bc0b340092c08fc3a20bcdf8f
jihyun <russee201916545@gmail.com>
2026-03-27 16:40:10 +0900

    feat: 01_dp_fibonacci

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -83,17 +83,22 @@ def fibonacci_memo(n, memo=None):
     Returns:
         n번째 피보나치 수
     """
-    # TODO: memo가 None이면 빈 딕셔너리로 초기화
-    pass
+    # TODO: memo가 None이면 빈 딕셔너리로 초기화 (저장할 공간 준비)
+    if memo is None:
+        memo = {}
     
-    # TODO: base case 
-    pass
+    # TODO: base case (피보나치는 base case 먼저 확인하는 것이 더 자연스러움)
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
     
-    # TODO: 이미 계산한 값이 memo에 있으면 반환
-    pass
+    # TODO: 이미 계산한 값이 memo에 있으면 반환 # key는 문제 번호(n) value는 그 답 fib(n)
+    if n in memo:
+        return memo[n]
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
-    pass
+    memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
     
     return memo[n]
 
@@ -116,5 +121,3 @@ if __name__ == "__main__":
     # 비교: Week1의 재귀 방식은 fib(50)을 계산하기 어려움
     print("참고: 일반 재귀는 fib(40)도 몇 초 걸리지만")
     print("메모이제이션은 fib(100)도 순식간에 계산!")
-
-
